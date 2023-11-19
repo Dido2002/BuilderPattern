2a01caec2e9f4c5d90daccae710ea29072b996d8
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-19 18:24:53 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBuilder
+{
+	class Product
+	{
+		public int Head { get; set; }
+		public int Body { get; set; }
+		public int Hands { get; set; }
+		public int Legs { get; set; }
+
+		public void PrintInfo()
+		{
+			Console.WriteLine("Head: {0}", Head);
+			Console.WriteLine("Body: {0}", Body);
+			Console.WriteLine("Hands: {0}", Hands);
+			Console.WriteLine("Legs: {0}", Legs);
+
+		}
+
+	}
+}
