dd8d4e1bb44565b881ea31f651896c650df37693
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-19 18:24:42 +0200

    Add files via upload

---
+++ OldRobotBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBuilder
+{
+	class OldRobotBuilder : Builder
+	{
+		public Product product = new Product();
+
+		public override void BuildHead()
+		{
+			product.Head = 1;
+		}
+		public override void BuildBody()
+		{
+			product.Body = 1;
+		}
+		public override void BuildHands()
+		{
+			product.Hands = 2;
+		}
+		public override void BuildLegs()
+		{
+			product.Legs = 2;
+		}
+		public override Product GetProduct()
+		{
+			return product;
+		}
+	}
+}
