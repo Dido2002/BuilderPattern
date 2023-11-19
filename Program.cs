049aead8091fc35c78e1769540fc132bc8de8021
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-19 18:25:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RobotBuilder
+{
+	public class Program
+	{
+
+		static void Main(string[] args) 
+		{
+			Director director = new Director();
+			Builder newRobotBuilder = new NewRobotBuilder();
+			director.Construct(newRobotBuilder);
+
+			newRobotBuilder.GetProduct().PrintInfo();
+
+			Builder oldRobotBuilder = new OldRobotBuilder();
+			director.Construct(oldRobotBuilder);
+
+			oldRobotBuilder.GetProduct().PrintInfo();
+
+			Console.ReadKey();
+		
+		}
+	}
+
+}
