6e15768410dd62ea05e40ce7fb583faeb7cf0dfa
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-19 18:24:01 +0200

    Add files via upload

---
+++ Builder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBuilder
+{
+	abstract class Builder
+	{
+		public abstract void BuildHead();
+		public abstract void BuildBody();
+		public abstract void BuildHands();
+
+		public abstract void BuildLegs();
+
+		public abstract Product GetProduct();
+	}
+}
